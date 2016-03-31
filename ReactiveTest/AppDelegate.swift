efc5045dedc7aecf2ac8c758666cb30bf4ab9b66
EVAN S. COMPTON <escompto@us.ibm.com>
2016-03-31 09:30:23 -0500

    Initial Commit

---
+++ ReactiveTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ReactiveTest
+//
+//  Created by Evan Compton on 3/31/16.
+//  Copyright © 2016 ibm. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ReactiveTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ReactiveTest
+//
+//  Created by Evan Compton on 3/31/16.
+//  Copyright © 2016 ibm. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
